836c8e05323be5d0da8e3387e657b8f3ba6dd27e
Maciej <51022993+MrCorncrake@users.noreply.github.com>
2021-11-02 14:25:24 +0100

    Updated character clearing regex and updated parsed versions of scenarios

--- src/main/java/utils/ParserRegex.java
+++ src/main/java/utils/ParserRegex.java
@@ -4,7 +4,7 @@ public abstract class ParserRegex {
     public final static String KEYWORD = "[A-Z][A-Z0-9']+( [A-Z][A-Z0-9'-]+|)+";
     public final static String CLEAR_TITLE = "\r\n|  +|--+|\"";
     public final static String CLEAR_AUTHOR = "  +|\r\n |\r\n";
-    public final static String CLEAR_CHARACTER = "\r\n(\t|  +)| \r\n|\r\n";
+    public final static String CLEAR_CHARACTER = "\r\n(\t|  +)| \r\n|\r\n| \\([^)]+\\)";
     public final static String AUTHOR_SPLIT = "[ \r\n]and| *& *|\\w+ by|\r\n";
     public final static String ON_SPLIT = "\r\nON [A-Z][A-Z0-9']+( [A-Z][A-Z0-9'-]+|)+";
     public final static String SENTENCE_SPLIT = "\r\n(  +)[A-Z][A-Z0-9'-]+( .*|)+(\r\n| \r\n)";
